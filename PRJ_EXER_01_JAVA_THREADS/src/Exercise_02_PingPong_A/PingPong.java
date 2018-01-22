8eb346b0b7b0533c0c8d669c6af9feca141e9483
Oriol Soler <oriol.soler.cabot@hotmail.com>
2018-01-22 18:34:03 +0100

    --

--- PRJ_EXER_01_JAVA_THREADS/src/Exercise_02_PingPong_A/PingPong.java
+++ PRJ_EXER_01_JAVA_THREADS/src/Exercise_02_PingPong_A/PingPong.java
@@ -2,61 +2,29 @@ package Exercise_02_PingPong_A;
 
 import java.util.concurrent.TimeUnit;
 
+
 public class PingPong {
 
-    public static void main(String[] args) throws InterruptedException {
-        Ping ping = new Ping(500);
-        Pong pong = new Pong(500);
+    public static void main (String [] args) {
+        Ping ping = new Ping();
+        Pong pong = new Pong();
 
-        ping.start();
-        pong.start();
-
-
-        Thread.sleep(5000);
-        ping.stop();
-        pong.stop();
+        /* COMPLETE */
     }
 
 }
 
 
 class Ping extends Thread {
-    private long timeToSleep;
 
-    public Ping(long timeToSleep) {
-        this.timeToSleep = timeToSleep;
-    }
-
-    @Override
-    public void run() {
+    public void run () {
         while (true) {
-            System.out.println("PING");
-            try {
-                Thread.sleep(timeToSleep);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            /* COMPLETE */
         }
     }
 }
 
 class Pong extends Thread {
-    private long timeToSleep;
 
-    public Pong(long timeToSleep) {
-        this.timeToSleep = timeToSleep;
-    }
-
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println("    PONG");
-            try {
-                Thread.sleep(timeToSleep);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
+    /* COMPLETE */
 }
